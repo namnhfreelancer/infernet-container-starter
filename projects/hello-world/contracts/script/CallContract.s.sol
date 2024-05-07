aba1c90f102499eb4c3e374043d94ba8a2f3ce40
ナム <namnhfreelancer@gmail.com>
2024-05-07 22:49:45 +0700

    feat: config for mainnet

--- projects/hello-world/contracts/script/CallContract.s.sol
+++ projects/hello-world/contracts/script/CallContract.s.sol
@@ -10,7 +10,7 @@ contract CallContract is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        SaysGM saysGm = SaysGM(0x663F3ad617193148711d28f5334eE4Ed07016602);
+        SaysGM saysGm = SaysGM(NEW_CONTRACT_AFTER_DEPLOYMENT);
 
         saysGm.sayGM();
 
--- projects/hello-world/contracts/script/Deploy.s.sol
+++ projects/hello-world/contracts/script/Deploy.s.sol
@@ -14,7 +14,7 @@ contract Deploy is Script {
         address deployerAddress = vm.addr(deployerPrivateKey);
         console2.log("Loaded deployer: ", deployerAddress);
 
-        address coordinator = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+        address coordinator = 0x8D871Ef2826ac9001fB2e33fDD6379b6aaBF449c;
         // Create consumer
         SaysGM saysGm = new SaysGM(coordinator);
         console2.log("Deployed SaysHello: ", address(saysGm));
